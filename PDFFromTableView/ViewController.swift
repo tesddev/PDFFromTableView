1a78c965f477719dce0db296134946ef87332399
tesddev <tesleem.amuda@gmail.com>
2022-08-25 12:08:50 +0100

    add download pdf functionality to project

--- PDFFromTableView/ViewController.swift
+++ PDFFromTableView/ViewController.swift
@@ -10,16 +10,33 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     @IBOutlet weak var detailsTableView: UITableView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         detailsTableView.delegate = self
         detailsTableView.dataSource = self
         detailsTableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
     @IBAction func didTapDownload(_ sender: Any) {
-        print("button tapped")
+        createPdfFromTableView()
+    }
+    
+    func createPdfFromTableView() {
+        let priorBounds: CGRect = self.detailsTableView.bounds
+        let fittedSize: CGSize = self.detailsTableView.sizeThatFits(CGSize(width: priorBounds.size.width, height: self.detailsTableView.contentSize.height))
+        self.detailsTableView.bounds = CGRect(x: 0, y: 0, width: fittedSize.width, height: fittedSize.height)
+        self.detailsTableView.reloadData()
+        let pdfPageBounds: CGRect = CGRect(x: 0, y: 0, width: fittedSize.width, height: (fittedSize.height))
+        let pdfData: NSMutableData = NSMutableData()
+        UIGraphicsBeginPDFContextToData(pdfData, pdfPageBounds, nil)
+        UIGraphicsBeginPDFPageWithInfo(pdfPageBounds, nil)
+        self.detailsTableView.layer.render(in: UIGraphicsGetCurrentContext()!)
+        UIGraphicsEndPDFContext()
+        let documentDirectories = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first
+        let documentsFileName = documentDirectories! + "/" + "BankStatement.pdf"
+        pdfData.write(toFile: documentsFileName, atomically: true)
+        print(documentsFileName)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
